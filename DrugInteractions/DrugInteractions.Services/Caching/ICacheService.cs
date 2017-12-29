ef4379a7f00507b08199cdc39f65a7c3fcbb48fa
Iliana Vachkova <an_vachkova@yahoo.com>
2017-12-29 18:37:02 +0200

    Added CacheService.cs(in memory cache).

---
+++ DrugInteractions/DrugInteractions.Services/Caching/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DrugInteractions.Services.Caching
+{
+    public interface ICacheService
+    {
+        T Get<T>(string cacheID, Func<T> getItemCallback) where T : class;
+
+        void Clear(string cacheId);
+    }
+}
---
+++ DrugInteractions/DrugInteractions.Services/Caching/Implementations/CacheService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DrugInteractions.Services.Caching.Implementations
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IMemoryCache cache;
+
+        public CacheService(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+
+        public T Get<T>(string cacheID, Func<T> getItemCallback) where T : class
+        {
+            T item = this.cache.Get(cacheID) as T;
+            if (item == null)
+            {
+                item = getItemCallback();
+                this.cache.Set(cacheID, item);
+            }
+            return item;
+        }
+
+        public void Clear(string cacheId)
+        {
+            this.cache.Remove(cacheId);
+        }
+    }
+}
