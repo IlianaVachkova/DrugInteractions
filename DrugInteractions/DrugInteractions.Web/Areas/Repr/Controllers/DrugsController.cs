5e5dbad66d2441d4a8ee30fb8bb93d4929792c31
Iliana Vachkova <an_vachkova@yahoo.com>
2017-12-30 23:55:26 +0200

    Added representative part for creating a drug.

---
+++ DrugInteractions/DrugInteractions.Web/Areas/Repr/Controllers/DrugsController.cs
@@ -0,0 +1,61 @@
+﻿using DrugInteractions.Data.Models.Users;
+using DrugInteractions.Services.Repr;
+using DrugInteractions.Web.Infrastructure.Populators;
+using DrugInteractions.Web.Areas.Repr.Models.Drugs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using DrugInteractions.Data.Models.Drugs;
+
+namespace DrugInteractions.Web.Areas.Repr.Controllers
+{
+    public class DrugsController : BaseReprController
+    {
+        private readonly IReprDrugsService reprDrugsService;
+
+        public DrugsController(IReprDrugsService reprDrugsService, UserManager<User> userManager, IDropDownListPopulator populator)
+            : base(userManager, populator)
+        {
+            this.reprDrugsService = reprDrugsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = new AddDrugFormModel
+            {
+                DrugGroups = await this.populator.GetDrugGroups(),
+                Brands = await this.populator.GetBrands()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddDrugFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.DrugGroups = await this.populator.GetDrugGroups();
+                model.Brands = await this.populator.GetBrands();
+                return View(model);
+            }
+
+            var dbModel = Mapper.Map<Drug>(model);
+
+            var currentUser = await userManager.GetUserAsync(HttpContext.User);
+            dbModel.Representative = currentUser;
+            dbModel.DateOfAddition = DateTime.UtcNow;
+
+            await this.reprDrugsService.CreateAsync(dbModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ DrugInteractions/DrugInteractions.Web/Areas/Repr/Models/Drugs/AddDrugFormModel.cs
@@ -0,0 +1,50 @@
+﻿using DrugInteractions.Common.Mapping;
+using DrugInteractions.Data.Models.Brands;
+using DrugInteractions.Data.Models.Drugs;
+using DrugInteractions.Data.Models.Users;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DrugInteractions.Web.Areas.Repr.Models.Drugs
+{
+    public class AddDrugFormModel : IMapFrom<Drug>
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ActiveSubstances { get; set; }
+
+        public string Excipients { get; set; }
+
+        public PharmaceuticalFormType PharmaceuticalForm { get; set; }
+
+        public string Usage { get; set; }
+
+        public string Description { get; set; }
+
+        public User Representative { get; set; }
+
+        public string RepresentativeId { get; set; }
+
+        public DateTime DateOfAddition { get; set; }
+
+        public DrugGroup DrugGroup { get; set; }
+
+        [Display(Name = "Drug Group")]
+        public int DrugGroupId { get; set; }
+
+        public Brand Brand { get; set; }
+
+        [Display(Name = "Brand")]
+        public int BrandId { get; set; }
+
+        public IEnumerable<SelectListItem> Brands { get; set; }
+
+        public IEnumerable<SelectListItem> DrugGroups { get; set; }
+    }
+}
